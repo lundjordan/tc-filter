fe3365a80a39ed0e6578d091ba00b1b117fbd32e
Jordan Lund <jlund@mozilla.com>
2019-10-08 13:28:46 -0700

    adds skeleton poetry structure

---
+++ tc_filter/tc-filter.py
@@ -0,0 +1,16 @@
+"""
+api:
+
+tc-filter groupid $groupid
+tc-filter taskid $taskid
+
+tc-filter groupid $groupid downstream
+tc-filter groupid $groupid downstream --hierarchy
+tc-filter groupid $groupid downstream --
+tc-filter taskid $taskid 
+"""
+
+def main():
+    pass
+
+__name__ == __main__ and main()
